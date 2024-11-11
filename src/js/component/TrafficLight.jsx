bbc4aa6091da0928f4c96b43fd1e554228341d78
LaHuella <german.dosaula@gmail.com>
2024-11-11 19:47:15 +0000

    Last Opt Missing

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function TrafficLight() {
+  const [color, setColor] = useState("red");
+  const toggleColor = () => {
+    if (color === "red") {
+      setColor("yellow");
+    } else if (color === "yellow") {
+      setColor("green");
+    } else {
+      setColor("red");
+    }
+  };
+
+  return (
+    <div className="full-screen">
+      <div className="traffic-light-container">
+        <div onClick={() => setColor("red")}className={`light red ${color === "red" ? "active" : ""}`}></div>
+        <div onClick={() => setColor("yellow")}className={`light yellow ${color === "yellow" ? "active" : ""}`}></div>
+        <div onClick={() => setColor("green")}className={`light green ${color === "green" ? "active" : ""}`}></div>
+      </div>
+      <button className="toggle-button" onClick={toggleColor}>Cambiar color</button>
+    </div>
+  );
+}
+
+export default TrafficLight;
