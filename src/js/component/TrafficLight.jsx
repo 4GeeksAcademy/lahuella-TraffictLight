56bc1e6925348299fa328ce6572b7f9465966247
LaHuella <german.dosaula@gmail.com>
2024-11-11 20:29:28 +0000

    Falta mejorar estilos

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -15,9 +15,9 @@ function TrafficLight() {
   return (
     <div className="full-screen">
       <div className="traffic-light-container">
-        <div onClick={() => setColor("red")}className={`light red ${color === "red" ? "active" : ""}`}></div>
-        <div onClick={() => setColor("yellow")}className={`light yellow ${color === "yellow" ? "active" : ""}`}></div>
-        <div onClick={() => setColor("green")}className={`light green ${color === "green" ? "active" : ""}`}></div>
+        <div onClick={() => setColor("red")}className={`light red ${color === "red" ? "on" : ""}`}></div>
+        <div onClick={() => setColor("yellow")}className={`light yellow ${color === "yellow" ? "on" : ""}`}></div>
+        <div onClick={() => setColor("green")}className={`light green ${color === "green" ? "on" : ""}`}></div>
       </div>
       <button className="toggle-button" onClick={toggleColor}>Cambiar color</button>
     </div>
